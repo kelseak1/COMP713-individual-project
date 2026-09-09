b8d1d0c825ed4bb132e6db2a185d072455b0bc9b
kelseak1 <kelseaking2005@gmail.com>
2026-09-10 03:26:00 +1200

    Create initial Spring Boot project

---
+++ community-centre-booking/community-centre-booking/src/main/java/nz/ac/aut/community_centre_booking/CommunityCentreBookingApplication.java
@@ -0,0 +1,13 @@
+package nz.ac.aut.community_centre_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityCentreBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityCentreBookingApplication.class, args);
+	}
+
+}
