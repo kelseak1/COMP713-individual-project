412be4fe26edc992f8d3987bee6116eb7e90f307
kelseak1 <kelseaking2005@gmail.com>
2026-09-19 22:59:57 +1200

    Add booking REST endpoints

---
+++ community-centre-booking/community-centre-booking/src/main/java/nz/ac/aut/community_centre_booking/controller/BookingController.java
@@ -0,0 +1,49 @@
+package nz.ac.aut.community_centre_booking.controller;
+
+import jakarta.validation.Valid;
+import nz.ac.aut.community_centre_booking.dto.BookingRequest;
+import nz.ac.aut.community_centre_booking.model.Booking;
+import nz.ac.aut.community_centre_booking.service.BookingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(
+            @Valid @RequestBody BookingRequest request) {
+
+        Booking createdBooking = bookingService.createBooking(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdBooking.getId())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(createdBooking);
+    }
+}
